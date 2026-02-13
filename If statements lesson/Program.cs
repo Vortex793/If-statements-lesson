e288c044b53c2f92342e15378afa23b8bf25e159
339821175 <339821175@2260I-L29-015>
2026-02-13 13:20:12 -0500

    Add project files.

---
+++ If statements lesson/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace If_statements_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int people = 20;
+            //int cats = 30;
+            //int dogs = 15;
+            //Console.WriteLine("People: " + people + "Dogs: " + dogs + "Cats: " + cats);
+            //if (people < cats)
+            //{
+            //    Console.WriteLine("Too many cats!   The world is doomed!");
+            //}
+            //if (people > cats)
+            //{
+            //    Console.WriteLine("Not many cats!   The world is saved");
+            //}
+            //if (people < dogs)
+            //{
+            //    Console.WriteLine("The world is drooled on");
+            //}
+            //if (people > dogs)
+            //{
+            //    Console.WriteLine("The world is dry!");
+            //}
+            //Console.WriteLine("Press ENTER to continue. ");
+            //Console.ReadLine();
+            //Console.Clear();
+            //dogs += 5;     // Add 5 to dogs. What does dogs equal now?
+            //Console.WriteLine("People: " + people + " Dogs: " + dogs + " Cats: " + cats);
+            //if (people >= dogs)
+            //{
+            //    Console.WriteLine("People are greater than or equal to dogs");
+            //}
+            //if (people <= dogs)
+            //{
+            //    Console.WriteLine("People are less than or equal to dogs");
+            //}
+            //if (people == dogs)
+            //{
+            //    Console.WriteLine("People are dogs.");
+
+            //string dinosaur;
+            //Console.WriteLine("What famous dinosaur has three large horns?");
+            //dinosaur = Console.ReadLine();
+
+            //if (dinosaur.ToLower() == "triceratops")
+            //    Console.WriteLine("You are correct!");
+
+            string magicWord;
+            Console.WriteLine("What's the magic word?");
+            magicWord = Console.ReadLine();
+            if
+
+
+        }
+    }
+    }
+
